59c4b09eb0f2b977cd3f2c814e5eed356aee4a6a
Suzy Song <zsong4321@gmail.com>
2025-07-08 15:06:53 +1000

    Delete: delete useless icon and links

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,11 +1,7 @@
 // src/App.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,24 +9,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://hono.dev/" target="_blank">
-          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-        </a>
-        <a href="https://workers.cloudflare.com/" target="_blank">
-          <img
-            src={cloudflareLogo}
-            className="logo cloudflare"
-            alt="Cloudflare logo"
-          />
-        </a>
-      </div>
       <h1>Vite + React + Hono + Cloudflare</h1>
       <div className="card">
         <button
