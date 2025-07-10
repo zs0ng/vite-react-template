049ded2253eba6def18884dc4f232c668cc4572b
Suzy Song <zsong4321@gmail.com>
2025-07-10 21:11:26 +1000

    feat: add Menu component with styles for menu items

---
+++ src/react-app/pages/menu/menu.tsx
@@ -0,0 +1,10 @@
+import "./menu.css";
+
+export default function Menu() {
+    return (
+        <div className="menu-container">
+            <div className="menu-item">Download</div>
+            <div className="menu-item">Web use</div>
+        </div>
+    )
+}
