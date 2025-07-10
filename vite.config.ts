1a314e3558b653a036a7c1ce3405ac9a5d7b7aaf
Suzy Song <zsong4321@gmail.com>
2025-07-10 15:01:11 +1000

    feat: remove suffix of dist assets by edit  "vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 });
