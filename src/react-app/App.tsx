7afc7c3a89b9fce15e5077e25929498100fd15d5
Suzy Song <zsong4321@gmail.com>
2025-07-10 16:31:22 +1000

    feat: add shiny text effect to description in landing page

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,19 +1,16 @@
 // src/App.tsx
-
 import "./App.css";
 
 function App() {
 
 
   return (
-    
     <div className="learnify-landing-page">
       <h1>
         <img className="logo learnify fade-in-up" 
         src="https://learnify-landing-page.pages.dev/assets/learnify-BydvyBxx.svg" 
         alt="Learnify" 
         style={{ height: "20px" }} 
-       
         />
       </h1>
       <p className="title fade-in-up delay-1" style={{ paddingTop: "90px"}}>LEARN EVERYTHING </p>
@@ -22,7 +19,7 @@ function App() {
       Build to help you master new skills, learn a vast amount of knowledge and even do a deep search.
       </p>
       <p className="description fade-in-up delay-3" style={{ color: "#838383", fontWeight: "300", paddingTop: "20px"}}>
-      Learnify is an powerful assistant that can inspire you, impress you and improve you.
+      <span className="shiny-text">Learnify is an powerful assistant that can inspire you, impress you and improve you.</span>
       </p>
       <button className="button fade-in-up delay-3" style={{ backgroundColor: "#000", color: "#ffffff", fontWeight: "300", padding: "10px 20px", borderRadius: "5px", marginTop: "20px" }}>Try it now</button>
     </div>
